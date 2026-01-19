b1d5877f2853428d2c50a5d3699643a997d388b4
sukhbirsinghmanhas-admin <sukhbirsinghmanhas@gmail.com>
2026-01-19 08:26:40 +0530

    New Testcase name

--- tests/myTest2.spec.ts
+++ tests/myTest2.spec.ts
@@ -1,7 +1,7 @@
 import {test,expect} from "@playwright/test"
 import { title } from "node:process";
 
-test ("Verify the URL",async ({page})=>{
+test ("Verify the URL 1",async ({page})=>{
 
    await page.goto("http://www.automationpractice.pl/index.php");
     let titleName:string=await page.title();
